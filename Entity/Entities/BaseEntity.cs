e03ec5d4276c4287e2503b7681f0b4dd999812b3
kienctfpoly <kienctph17436@fpt.edu.vn>
2022-10-10 23:43:12 +0700

    Thêm 1 vài project phục vụ sau này

---
+++ Entity/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopTheThao.Entity.Entities.Enum;
+
+namespace ShopTheThao.Entity.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public Status Status { get; set; }
+    }
+}
---
+++ Entity/Entities/Enum/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTheThao.Entity.Entities.Enum
+{
+    public enum Status
+    {
+        Active = 1,
+        InActive = 0,
+        IsDeleted = 2,
+    }
+}
